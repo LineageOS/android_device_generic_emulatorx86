2eeb34149e2249c5ffa7bc985fbff3f7bca49873
Chris Sarbora <sarbs@cyngn.com>
2015-01-21 13:52:21 -0800

    WIP Package up a system image
     * DO NOT SUBMIT: Only produces images for linux host-os currently.
       Working to figure out how to produce a host-agnostic product.
    
    Change-Id: I63ea6303f1129a23bb8dbdc0dd6bbb9144f9df5c

--- cm.mk
+++ cm.mk
@@ -9,4 +9,13 @@ PRODUCT_DEVICE := emulatorx86
 PRODUCT_BRAND := CyanogenMod
 PRODUCT_MODEL := Full CyanogenMod on x86 Emulator
 
+PRODUCT_SDK_ADDON_NAME := cm12emu
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
+
+# Copy the manifest and hardware files for the SDK add-on.
+# The content of those files is manually created for now.
+PRODUCT_SDK_ADDON_COPY_FILES := \
+    $(LOCAL_PATH)/sdk_addon/manifest.ini:manifest.ini \
+    $(LOCAL_PATH)/sdk_addon/hardware.ini:hardware.ini \
+
 bacon: droid
