ee2154ce38c7e7128b9da88b1babe7d0de301a59
Chris Sarbora <sarbs@cyngn.com>
2015-01-21 13:52:21 -0800

    Package up a system image
    Change-Id: I63ea6303f1129a23bb8dbdc0dd6bbb9144f9df5c

--- cm.mk
+++ cm.mk
@@ -9,4 +9,18 @@ PRODUCT_DEVICE := emulatorx86
 PRODUCT_BRAND := CyanogenMod
 PRODUCT_MODEL := Full CyanogenMod on x86 Emulator
 
+PRODUCT_SDK_ADDON_NAME := cm12emu
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
+
+# Copy the manifest and hardware files for the SDK add-on.
+# The content of those files is manually created for now.
+PRODUCT_SDK_ADDON_COPY_FILES := \
+    $(LOCAL_PATH)/sdk_addon/manifest.ini:manifest.ini \
+    $(LOCAL_PATH)/sdk_addon/hardware.ini:hardware.ini \
+
+
+LOCAL_KERNEL := prebuilts/qemu-kernel/x86/kernel-qemu
+PRODUCT_SDK_ADDON_COPY_FILES += \
+    $(LOCAL_KERNEL):images/x86/kernel-qemu
+
 bacon: droid
