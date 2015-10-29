0db24873af56e8aa6ee6bbacf5bf35dec4ff656c
Chris Sarbora <sarbs@cyngn.com>
2015-10-29 16:25:29 -0700

    WIP build goldfish kernel along with device
    Change-Id: I4e051700fb46e5c0575c72ad42f238e78991f070

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,9 +6,19 @@
 # same as x86 except HAL
 include device/generic/x86/BoardConfig.mk
 
-TARGET_KERNEL_SOURCE := prebuilts/qemu-kernel/x86
-TARGET_KERNEL_CONFIG := cyanogenmod_emulatorx86_x86_defconfig
-TARGET_PREBUILT_KERNEL := prebuilts/qemu-kernel/x86/kernel-qemu
+# Define kernel config for inline building
+#KERNEL_TOOLCHAIN := external/qemu/distrib/kernel-toolchain
+KERNEL_TOOLCHAIN_PREFIX := $(abspath external/qemu/distrib/kernel-toolchain/android-kernel-toolchain-)
+KERNEL_REAL_TOOLCHAIN_PREFIX := $(abspath prebuilts/gcc/linux-x86/x86/x86_64-linux-android-4.8/bin/x86_64-linux-android-)
+BOARD_KERNEL_IMAGE_NAME := bzImage
+TARGET_NO_KERNEL :=
+TARGET_NO_RAMDISK :=
+TARGET_KERNEL_CONFIG := goldfish_defconfig
+TARGET_KERNEL_SOURCE := kernel/goldfish
+
+# TARGET_KERNEL_CONFIG := cyanogenmod_emulatorx86_x86_defconfig
+# TARGET_KERNEL_SOURCE := prebuilts/qemu-kernel/x86
+# TARGET_PREBUILT_KERNEL := prebuilts/qemu-kernel/x86/kernel-qemu
 
 # Build OpenGLES emulation libraries
 BUILD_EMULATOR_OPENGL := true
